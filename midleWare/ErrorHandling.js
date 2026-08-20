cb0d771c0cfb2a8c1e8280db758574f9c6b6d4ca
Radwa Walid <radwawalid503@gmail.com>
2026-08-20 21:32:21 +0300

    organize backend and integrate courses

--- midleWare/ErrorHandling.js
+++
@@ -1,6 +0,0 @@
-function ErrorHandle(err, req, res, next) {
-  console.log("somthing broken", err);
-
-  res.status(500).json({ msg: "somthing broken ! , try again " });
-}
-module.exports = { ErrorHandle };
